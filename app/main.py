28dbd2218573b6424c59d019e5a85d15a83fe434
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-06-18 19:06:12 +0500

    CORS URL ADDED PORTFOLIO

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://portfolio-chat-bot-two.vercel.app" , "https://innovatesoftwaresolution.com" , "http://localhost:3000"],
+    allow_origins=["https://portfolio-chat-bot-two.vercel.app" , "https://innovatesoftwaresolution.com" , "http://localhost:3000" , "https://practice-nextjs-iota-one.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
